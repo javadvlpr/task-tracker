553da01ed3d609aad9e1fbaab70b72f7e375cda3
whyAbu <abdurahmon_imomnazarov@student.itpu.uz>
2025-06-24 18:49:23 +0500

    porject created by using Spring boot

---
+++ src/test/java/uz/itpu/tasktracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.itpu.tasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
