b4ff234105c306ca9c5f23c1697577497be184c0
whyAbu <abdurahmon_imomnazarov@student.itpu.uz>
2025-06-25 10:18:39 +0500

    fixed all bugs and CRUD operation completed succesfully

--- src/main/java/uz/itpu/tasktracker/controller/TaskController.java
+++ src/main/java/uz/itpu/tasktracker/controller/TaskController.java
@@ -1,7 +1,7 @@
 package uz.itpu.tasktracker.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +14,19 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/task")
-@Api(tags = "Task Endpoints")
+//@Api(tags = "Task Endpoints")
 public class TaskController {
     private final TaskService taskService;
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("Create Task")
+//    @ApiOperation("Create Task")
     public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
         taskDto.setId(null);
         return ResponseEntity.ok(taskService.createTask(taskDto));
     }
 
     @PutMapping("/update/{id}")
-    @ApiOperation("Update Task by id")
+//    @ApiOperation("Update Task by id")
     public ResponseEntity<TaskDto> updateTask(@PathVariable Integer id, @RequestBody TaskDto taskDto) {
         taskDto.setId(null);
         return ResponseEntity.ok(taskService.updateTask(id, taskDto));
@@ -34,19 +34,19 @@ public class TaskController {
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @ApiOperation("Delete Task by id")
+//    @ApiOperation("Delete Task by id")
     public ResponseEntity<String> deleteTask(@PathVariable Integer id) {
         return ResponseEntity.ok(taskService.deleteTask(id));
     }
 
     @GetMapping("/get-by-id/{id}")
-    @ApiOperation("Get Task by id")
+//    @ApiOperation("Get Task by id")
     public ResponseEntity<TaskDto> getTask(@PathVariable Integer id) {
         return ResponseEntity.ok(taskService.getTask(id));
     }
 
     @GetMapping("/get-all")
-    @ApiOperation("Get list of all Tasks")
+//    @ApiOperation("Get list of all Tasks")
     public ResponseEntity<List<TaskDto>> getAllTask() {
         return ResponseEntity.ok(taskService.getAllTask());
     }
