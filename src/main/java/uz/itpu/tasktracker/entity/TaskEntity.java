7adb99ec36c9d521081a2a71023861d320c54541
whyAbu <abdurahmon_imomnazarov@student.itpu.uz>
2025-07-08 12:24:41 +0500

    changed TaskEntity.java

--- src/main/java/uz/itpu/tasktracker/entity/TaskEntity.java
+++ src/main/java/uz/itpu/tasktracker/entity/TaskEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.Id;
 import uz.itpu.tasktracker.enums.Priority;
 import uz.itpu.tasktracker.enums.Status;
@@ -37,6 +38,7 @@ public class TaskEntity {
     @Enumerated(EnumType.ORDINAL)
     private Priority priority;
 
+    @CreationTimestamp
     @Column(name = "created_datetime")
     private LocalDateTime createDate;
 
